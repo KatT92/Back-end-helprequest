9ae652374a34757fbcd411e09f16eb4a30b2efff
Katherine Tweeddale <Katweeddale@gmail.com>
2022-01-18 15:24:51 +0000

    env variables

---
+++ api/data.js
@@ -0,0 +1,8 @@
+let data = [
+    {fname:"a", lname:"e", room:1, problem:"asd", tried:"rty"},
+    {fname:"b", lname:"f", room:2, problem:"fgh", tried:"uio"},
+    {fname:"c", lname:"g", room:3, problem:"jkl", tried:"zxc"},
+    {fname:"c", lname:"h", room:4, problem:"qwe", tried:"vbn"}
+]
+
+export default data
--- api/db/connection.js
+++ api/db/connection.js
@@ -1,5 +1,13 @@
 import pg from "pg";
+import { databaseHost, databaseName, username, dbPort, password } from "../config";
 
-const pool = new pg.Pool();
+const pool = new pg.Pool(
+    {
+    host: databaseHost,
+    database: databaseName,
+    user: username,
+    port: dbPort,
+    password: password,
+});
 
 export default pool;
