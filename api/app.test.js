a7c96ab80e1ec01db14720343cde1bbb34ea0424
Immanuel512 <immanuelabiodun07@gmail.com>
2022-01-19 12:05:03 +0000

    api-routes

--- api/app.test.js
+++ api/app.test.js
@@ -5,7 +5,7 @@ import app from "./app.js";
 describe("GET /users", function () {
   test("gives us back 200, with a message", async function () {
     const expectedBody = {
-      message: "I wish we had some information to give you ☹️"
+      message: "I wish we had some information to give you ☹️",
     };
     const actual = await request(app).get("/users");
     expect(actual.body).toStrictEqual(expectedBody);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,15 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-// import { getAllData } from "../models/users.js";
-import helpData from "../helpData.js";
+import { getAllData } from "../models/users.js";
 
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
   console.log("Running...");
 
-  // const allHelpData = await getAllData();
-  res.json({ success: true, payload: "message" });
+  const allHelpData = await getAllData();
+  res.json({ success: true, payload: allHelpData });
 });
 
 export default router;
