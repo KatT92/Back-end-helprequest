938889a772b700f7279707533310b49a7013ffc9
Katherine Tweeddale <Katweeddale@gmail.com>
2022-02-26 19:57:44 +0000

    upload

--- db/index.js
+++ db/index.js
@@ -1,13 +1,7 @@
 import pg from "pg";
 
 const pool = new pg.Pool({
-  connectionString:
-    "postgres://whsrdosnytuazf:b23712db6f28bf784e59837a85281f23ab6bdbd9070261862f9285036960a2de@ec2-34-246-155-237.eu-west-1.compute.amazonaws.com:5432/d5aee003nu79j4",
-  // host: databaseHost,
-  // database: databaseName,
-  // user: username,
-  // port: dbPort,
-  // password: password,
+  connectionString: process.env.db_url,
   ssl: { rejectUnauthorized: false },
 });
 
